99328c7bd98eefdc294dceff1ed3d1763dc1a300
Eric <xiaoxiang.cn@gmail.com>
2016-11-29 10:04:07 +0800

    Release v1.1

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ from setuptools import setup
 
 
 version = {"name": 'platform',
-           "version": '1.0',
+           "version": '1.1',
            "description": 'Cereson Platform.',
            }
 
